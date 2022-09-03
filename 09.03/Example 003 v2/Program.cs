6f2acb94363517c21dd8df13272ace51134aee77
Andrey <ab18021999@mail.ru>
2022-09-04 00:25:47 +0300

    Правильное решение задачи 3

--- 09.03/Example 003 v2/Program.cs
+++ 09.03/Example 003 v2/Program.cs
@@ -7,30 +7,18 @@ int Prompt(string message)
     int value = int.Parse(strValue);      // Преобразование строки в целое число
     return value;
 }
-int number = Prompt("Введите число от 100 до 100000:");
-if (number > 99 && number < 1000)
+int number = Prompt("Введите число от 100:");
+
+if (number > 99)
 {
+    while (number > 999)
+    {
+        number = number / 10;
+    }
     int result = number % 10;
-    System.Console.WriteLine(result);
-}
-if (number > 999 && number < 10000)
-{
-    int result = number / 10 % 10;
-    System.Console.WriteLine(result);
-}
-if (number > 9999 && number < 100000)
-{
-    int result = number / 100 % 10;
     System.Console.WriteLine(result);
 }
 else
 {
-    if (number < 99)
-    {
-        System.Console.WriteLine("Число либо меньше 100, третьей цифры нет.");
-    }
-    else
-    {
-        System.Console.WriteLine("Число больше 100000");
-    }
+    System.Console.WriteLine("Число либо меньше 100, третьей цифры нет.");
 }
