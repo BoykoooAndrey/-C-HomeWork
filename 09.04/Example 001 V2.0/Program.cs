8eb85abe9e16c5591a04db98af3dadca82a7805d
Andrey <ab18021999@mail.ru>
2022-09-05 21:33:42 +0300

    выполнено дз к семинару 04.09

---
+++ 09.04/Example 001 V2.0/Program.cs
@@ -0,0 +1,66 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 121 -> да
+// 2332 -> да
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число:");
+int Check(int number)
+{
+    if (number < 0)
+    {
+        number = number * -1;
+    }
+    return number;
+}
+number = Check(number);
+System.Console.WriteLine(number);
+int Size(int number)
+{
+    int length = Convert.ToString(number).Length;
+    return length;
+}
+int length = Size(number);
+int[] Array(int number, int length)
+{
+    int[] numbers = new int[length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = number % 10;
+        number = number / 10;
+    }
+    return numbers;
+}
+int[] numbers = Array(number, length);
+bool Palindrome(int[] numbers)
+{
+    bool result = true;
+    for (int i = 0; i < numbers.Length / 2; i++)
+    {
+        if (numbers[i] == numbers[^(i + 1)])
+            result = true;
+        else
+        {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
+}
+bool result = Palindrome(numbers);
+void Print(bool result, int number)
+{
+    if (result == true)
+    {
+        System.Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+Print(result, number);
---
+++ 09.04/Example 001/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите пятизначное число:");
+int Palindrome(int number)
+{
+    int result = 0;
+    if (number > 9999 && number < 100000)
+    {
+        int number1 = number / 10000;
+        int number2 = number / 1000 % 10;
+        int number4 = number / 10 % 10;
+        int number5 = number % 10;
+        if (number1 == number5 && number2 == number4)
+        {
+            result = 1;
+
+        }
+        else
+        {
+            result = 0;
+        }
+
+    }
+    else
+    {
+        result = 2;
+    }
+    return result;
+}
+int result = Palindrome(number);
+void Print(int result, int number)
+{
+    if (result == 0)
+    {
+        System.Console.WriteLine($"Число {number} не является палиндромом");
+    }
+    if (result == 1)
+    {
+        System.Console.WriteLine($"Число {number} является палиндромом");
+    }
+    if (result == 2)
+    {
+        System.Console.WriteLine($"Число {number} не пятизначное");
+    }
+}
+Print(result, number);
